181af73d25f309b710cbd8281c77770931cf5d86
Anthony Pileggi <adpileggi@gmail.com>
2024-03-15 11:03:41 -0400

    filled out index.js model

--- models/index.js
+++ models/index.js
@@ -5,13 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 // Categories have many Products
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
-
+ProductTag.belongsToMany(Tag, {
+  foreignKey: 'tag_id'
+})
 // Tags belongToMany Products (through ProductTag)
-
+ProductTag.belongsToMany(Product, {
+  foreignKey: 'product_id'
+})
 module.exports = {
   Product,
   Category,
